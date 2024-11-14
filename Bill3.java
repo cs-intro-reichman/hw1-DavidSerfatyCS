a9453399aa039e01a0e0c1a86ed5f0fd605a46b1
David Serfaty <davidserfaty11@gmail.com>
2024-11-14 18:00:22 +0200

    Bill3 fixed

--- Bill3.java
+++ Bill3.java
@@ -4,21 +4,19 @@ public class Bill3 {
     String name1 = args[0];
     String name2 = args[1];
     String name3 = args[2];
+
     double value = Double.parseDouble(args[3]); 
-
     int nOfDiners = 3;
+    double bill3 = (value/nOfDiners);
+    bill3 = Math.ceil(bill3);
 
-    System.out.println(
-        name1  + " " + 
-        name2  + " " + 
-        name3  + " " +
-        value);
-
-    double bill3 = Math.ceil(value/nOfDiners);
-
-    System.out.println(
-    "Each one should pay: " +
-     bill3 + " Shekels. ");
+    System.out.println( 
+        "Dear " +
+        name1   + ", "     + 
+        name2   + ", and " + 
+        name3   + ": pay " +
+        bill3   + 
+        "Shekels each." );
 
 
     }
